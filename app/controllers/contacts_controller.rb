0f0c23fc6200db1ef07f984e1359c297e514f873
LEARN01 <learn01@LEARN01.local>
2015-01-30 13:53:47 -0800

    completed crud - still ugly

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,56 @@
 class ContactsController < ActionController::Base
+  layout 'application'
 
   def index
     @contacts = Contact.all
-    render('contacts/index.html.erb')
+    # render('contacts/index.html.erb')
   end
 
   def show
     @contact = Contact.find(params[:id])
-    render('contacts/show.html.erb')
+    # render('contacts/show.html.erb')
+  end
+
+  def new
+    @contact = Contact.new
+    # render('contacts/new.html.erb')
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    # render('contacts/edit.html.erb')
+  end
+
+  def create
+    @contact = Contact.create(
+    :name => params[:name],
+    :email => params[:email],
+    :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.name = params[:name]
+    @contact.email = params[:email]
+    @contact.phone = params[:phone]
+    if @contact.save
+      @contacts = Contact.all
+      render('contacts/index.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    @contacts = Contact.all
+    render('contacts/index.html.erb')
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  match('contacts', {:via => :get, :to => 'contacts#index'})
-  match('contact/:id', {:via => :get, :to => 'contacts#show'})
+  resources :contacts
+  # match('contacts/', {:via => :get, :to => 'contacts#index'})
+  # match('contacts/', {:via => :post, :to => 'contacts#create'})
+  # match('contacts/', {:via => :delete, :to => 'contacts#destroy'})
+  # match('contacts/:id/edit', {:via => :get, :to => 'contacts#edit'})
+  # match('contacts/:id', {:via => :get, :to => 'contacts#show'})
+  # match('contacts/:id', {:via => [:patch, :put],  :to => 'contacts#update'})
 end
